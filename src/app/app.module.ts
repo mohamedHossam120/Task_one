cc0fd590903d9966394e9f323d76b374dc19ae2d
mohamedHossam120 <mohamedhossameldeen12@gmail.com>
2025-08-16 17:27:54 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { WorkComponent } from './work/work.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    WorkComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
